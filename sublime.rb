7205d951c8f10ed3ce4dac49f66316ecad5ca3a1
calamari <g.tavonius@gmail.com>
2013-08-07 23:27:49 +0200

    Fixed installing of Sublime Text 2

--- sublime.rb
+++ sublime.rb
@@ -1,5 +1,7 @@
 dep 'sublime.app' do
   source 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
+  provides 'Sublime Text 2.app'
+  version '0'
 end
 
 dep 'subl', :template => 'sym_link' do
