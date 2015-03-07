f99e7408d9bc61fb26738c75285d4c07d0269b59
Calamari <g.tavonius@gmail.com>
2015-03-07 12:47:35 +0100

    Update ccmenu version to 1.8.1

--- ccmenu.rb
+++ ccmenu.rb
@@ -1,5 +1,5 @@
 dep 'ccmenu.app' do
-  source 'http://downloads.sourceforge.net/project/ccmenu/CCMenu/1.6.3/ccmenu-1.6.3-b.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fccmenu%2Ffiles%2FCCMenu%2F&ts=1376302636&use_mirror=netcologne'
+  source 'http://downloads.sourceforge.net/project/ccmenu/CCMenu/1.8.1/ccmenu-1.8.1-b.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fccmenu%2Ffiles%2FCCMenu%2F&ts=1376302636&use_mirror=netcologne'
 
   provides 'CCMenu.app'
 end
