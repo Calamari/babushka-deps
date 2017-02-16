1b9e79e65f547be4a8d47af7dc88dd74dcab68d7
Calamari <g.tavonius@gmail.com>
2017-02-16 21:45:36 +0100

    Add some more mac apps

--- mac_sources.rb
+++ mac_sources.rb
@@ -6,8 +6,20 @@ dep "Alfred.app" do
   source "https://cachefly.alfredapp.com/Alfred_3.3_801.dmg"
 end
 
+dep 'Android File Transfer.app' do
+  source 'https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg'
+end
+
 dep 'CCMenu.app' do
   source 'http://downloads.sourceforge.net/project/ccmenu/CCMenu/1.8.1/ccmenu-1.8.1-b.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fccmenu%2Ffiles%2FCCMenu%2F&ts=1376302636&use_mirror=netcologne'
+end
+
+dep 'CLIQZ.app' do
+  source 'https://cdn.cliqz.com/browser-f/download/web0001/CLIQZ.en-US.mac.dmg'
+end
+
+dep 'Disk Inventory.app' do
+  source 'http://www.derlien.com/diskinventoryx/downloads/dev/DIX1.0Universal.dmg'
 end
 
 dep "Dropbox.app" do
@@ -16,6 +28,16 @@ end
 
 dep "Evernote.app" do
   source "https://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_6.10_454267.dmg"
+end
+
+dep "Google Chrome.app" do
+  source "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+end
+
+dep 'ImageOptim', :version, template: 'src' do
+  version.default!('1.6.5')
+  source "https://github.com/ImageOptim/ImageOptim/archive/#{version}.tar.gz"
+  provides "ImageOptim.app ~> #{version}"
 end
 
 dep "KeepassX.app" do
@@ -54,6 +76,10 @@ dep "VirtualBox.app" do
   source "http://download.virtualbox.org/virtualbox/4.3.18/VirtualBox-4.3.18-96516-OSX.dmg"
 end
 
+dep 'Vivaldi.app' do
+  source 'https://downloads.vivaldi.com/stable/Vivaldi.1.7.735.46.dmg'
+end
+
 dep "VLC.app" do
   source "http://get.videolan.org/vlc/2.1.5/macosx/vlc-2.1.5.dmg"
 end
@@ -62,12 +88,18 @@ dep "macOS Apps" do
   requires \
     'Adium.app',
     'Alfred.app',
+    'Android File Transfer.app',
     'CCMenu.app',
+    'CLIQZ.app',
+    'Disk Inventory.app',
     'Dropbox.app',
     'Evernote.app',
+    'Google Chrome.app',
+    'ImageOptim',
     'KeepassX.app',
     'Postgres.app',
     'Slack',
     'VirtualBox.app',
+    'Vivaldi.app',
     'VLC.app'
 end
