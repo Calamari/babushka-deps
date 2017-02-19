71077251a55965bb4fa319c3a538b79f52ca2913
Calamari <g.tavonius@gmail.com>
2017-02-19 20:28:24 +0100

    Install elixir and erlang prerequisite

--- languages.rb
+++ languages.rb
@@ -1,12 +1,17 @@
-dep 'erlang.app' do
-  source 'https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.2-1~osx~10.10_amd64.dmg'
+# dep 'erlang.app' do
+#   source 'https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.2-1~osx~10.10_amd64.dmg'
+#
+#   provides 'erl'
+# end
 
+dep 'erlang.managed' do
+  requires_when_unmet 'homebrew'
   provides 'erl'
 end
 
 dep 'elixir.managed' do
-  requires 'homebrew'
-  requires 'erlang.app'
+  requires_when_unmet 'homebrew'
+  requires 'erlang.managed'
 
   provides 'elixir'
 end
