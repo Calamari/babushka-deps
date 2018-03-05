b56be06569f5e263078cf7f5839de1c7cc662c86
Calamari <g.tavonius@gmail.com>
2018-03-05 10:28:07 +0100

    We use sublime text 3 again. Atom is so annoying slow and bloated

--- sublime.rb
+++ sublime.rb
@@ -1,39 +1,11 @@
-dep 'sublime.app' do
-  source 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
-  provides 'Sublime Text 2.app'
-  version '0'
+dep 'Sublime Text.app' do
+  source 'https://download.sublimetext.com/Sublime%20Text%20Build%203143.dmg'
 end
 
-dep 'subl', :template => 'sym_link' do
-  requires 'sublime.app'
+dep 'subl', template: 'sym_link' do
+  requires 'Sublime Text.app'
 
-  source '/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+  source '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
   destination '/usr/local/bin/subl'
   use_sudo true
-end
-
-dep 'sublime package control' do
-  requires 'wget.managed'
-  package_name = "Package\ Control.sublime-package"
-  dir = "#{ENV['HOME']}/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages/"
-  source = "http://sublime.wbond.net/Package%20Control.sublime-package"
-
-  met? {
-    "#{dir}#{package_name}".p.exists?
-  }
-  meet {
-    shell "wget --directory-prefix='#{dir}' #{source}"
-  }
-end
-
-dep 'sublime bundler' do
-  export_string = 'export BUNDLER_EDITOR=subl'
-
-  met? {
-    shell("cat #{ENV['HOME']}/.bash_profile").include?(export_string)
-  }
-
-  meet {
-    log_shell "Add subl as $BUNDLER_EDITOR", "echo '#{export_string}' >> ~/.bash_profile"
-  }
 end
